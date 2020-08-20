bad405f1ebe389c3c6477ecf1cd854025e9cbd38
rigoriosh <rigoriosh@gmail.com>
2020-08-20 16:32:24 -0500

    inicio componente nav Bar

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,25 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <encabezado />
+    <contenido />
+    <pieDePagina />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import encabezado from "./components/encabezado";
+import contenido from "./components/contenido";
+import pieDePagina from "./components/pieDePagina";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    encabezado,
+    contenido,
+    pieDePagina
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/contenido.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>este va hacer el contenido si este es en serio</div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+
+<style scoped>
+</style>
